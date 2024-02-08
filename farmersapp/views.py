64fb80fa2b4a9c434a93fe4b5cd08ccaf37ff0b9
carzoongu <bka1739@gmail.com>
2024-02-08 10:45:59 +0300

    commit changes

--- farmersapp/views.py
+++ farmersapp/views.py
@@ -9,7 +9,7 @@ import urllib.request
 import json
 
 
-openai_api_key = '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'
+openai_api_key = ''
 openai.api_key = openai_api_key
 
 
@@ -22,7 +22,7 @@ def ask_openai(message):
         #stop=None,
         #temperature=0.7,
         messages=[
-            {"role": "system", "content": "You are a farming expert."},
+            {"role": "system", "content": "You are an agriculture expert, you only answer questions related to agriculture only. Yoy ignore the rest of the questions by giving a polite response. You can respond to greeting and weather forecast related questions"},
             {"role": "user", "content": message},
         ]
     )
@@ -61,12 +61,17 @@ def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         
         if password1 == password2:
             try:
                 user = User.objects.create_user(username, email, password1)
+                user.first_name = fname
+                user.last_name = lname
+                
                 user.save()
                 auth.login(request, user)
                 return redirect('dashboard')
@@ -77,6 +82,25 @@ def register(request):
             error_message = 'Passwords dont match'
             return render(request, 'register.html', {'error_message': error_message})
     return render(request, 'register.html')
+        #if User.objects.filter(username=username):
+            #error_message = 'Username already exists'
+            #return render(request, 'register.html', {'error_message': error_message})
+        #if password1 != password2:
+            #error_message = 'Passwords do not match'
+            #return render(request, 'register.html', {'error_message': error_message})
+        #if User.objects.filter(email=email):
+            #error_message ='Email already exist'
+            #return render(request, 'register.html', {'error_message': error_message})
+        
+        #user = User.objects.create_user(username, email, password1)
+        #user.first_name = fname
+        #user.last_name = lname
+           
+        #user.save()
+        #auth.login(request, user)
+        #return redirect('dashboard')
+        
+            
 
 def logout(request):
     auth.logout(request)
